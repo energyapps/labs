cf22bdf6996b987eec69d474427c695ccc5efdfa
Daniel Wood <daniel.j.wood11@gmail.com>
2014-03-27 13:23:47 -0400

    init commit

---
+++ doe_facilities.js
@@ -0,0 +1,422 @@
+var points = [
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -93.648021,
+          42.029874
+        ]
+      },
+      "properties": {
+        "id": "1",
+        "facility": "Ames Lab",
+        "city": "Ames",
+        "state": " Iowa",
+        "year": "1947",
+        "employ": "450",
+        "txt": "Materials science and engineering; chemical and biological sciences; applied math and computational science; environmental and protection sciences; and simulation, modeling and decision science.",
+        "URL": "https://www.ameslab.gov/",
+        "facility2": "Ames Laboratory"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -87.971398,
+          41.711175
+        ]
+      },
+      "properties": {
+        "id": "2",
+        "facility": "Argonne National Lab",
+        "city": "Lemont",
+        "state": " Illinois",
+        "year": "1946",
+        "employ": "3,500",
+        "txt": "Physics, chemistry, biological sciences, energy storage, high-performance computing, national security, engines and alternative fuels, environmental science and nuclear energy",
+        "URL": "http://www.anl.gov/",
+        "facility2": "Argonne National Laboratory"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -72.8808398,
+          40.869162
+        ]
+      },
+      "properties": {
+        "id": "3",
+        "facility": "Brookhaven National Lab",
+        "city": "Upton",
+        "state": " New York",
+        "year": "1947",
+        "employ": "3,000",
+        "txt": "Nuclear and particle physics; photon sciences and nano materials; and cross disciplinary research on climate change, sustainable energy and Earth's ecosystems",
+        "URL": "http://www.bnl.gov/world/",
+        "facility2": "Brookhaven National Laboratory"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -88.255419,
+          41.832671
+        ]
+      },
+      "properties": {
+        "id": "4",
+        "facility": "Fermi National Accelerator Lab",
+        "city": "Batavia",
+        "state": " Illinois",
+        "year": "1967",
+        "employ": "1,700",
+        "txt": "Particle physics and accelerator science and technology",
+        "URL": "http://www.fnal.gov/",
+        "facility2": "Fermi National Accelerator Laboratory"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -112.0456941,
+          43.5171912
+        ]
+      },
+      "properties": {
+        "id": "5",
+        "facility": "Idaho National Lab",
+        "city": "Idaho Falls",
+        "state": "Idaho",
+        "year": "1949",
+        "employ": "3,500",
+        "txt": "Nuclear science and engineering; national security research and testing; energy and environmental sustainability",
+        "URL": "https://inlportal.inl.gov/portal/server.pt/community/home/255",
+        "facility2": "Idaho National Laboratory"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.2536109,
+          37.8750034
+        ]
+      },
+      "properties": {
+        "id": "6",
+        "facility": "Lawrence Berkeley National Lab",
+        "city": "Berkeley",
+        "state": " California",
+        "year": "1931",
+        "employ": "4,000",
+        "txt": "Physics; chemistry; nuclear science; accelerator research; photon science and engineering sciences; computational research and mathematics; Earth sciences; energy efficiency; materials sciences; and life sciences, genomics, and physical biosciences",
+        "URL": "http://www.lbl.gov/",
+        "facility2": "Lawrence Berkeley National Laboratory"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -121.7211155,
+          37.6798239
+        ]
+      },
+      "properties": {
+        "id": "7",
+        "facility": "Lawrence Livermore National Lab",
+        "city": "Livermore",
+        "state": " California",
+        "year": "1952",
+        "employ": "6,500",
+        "txt": "Nuclear weapons stockpile stewardship, nuclear nonproliferation, high-performance computing, national security, biology, energy research, climate science, additive manufacturing, lasers and high-energy-density physics",
+        "URL": "https://www.llnl.gov/",
+        "facility2": "Lawrence Livermore National Laboratory"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -106.334031,
+          35.900731
+        ]
+      },
+      "properties": {
+        "id": "8",
+        "facility": "Los Alamos National Lab",
+        "city": "Los Alamos",
+        "state": " New Mexico",
+        "year": "1943",
+        "employ": "10,000",
+        "txt": "Accelerators and electrodynamics; bioscience, biosecurity and health; chemical science; Earth and space sciences; energy; engineering; high-energy-density plasmas and fluids; information science, computing and applied math; materials science; national security and weapons science; nuclear and particle physics; astrophysics and cosmology; and sensors and information systems",
+        "URL": "http://www.lanl.gov/index.php",
+        "facility2": "Los Alamos National Laboratory"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -79.9770211,
+          39.6646947
+        ]
+      },
+      "properties": {
+        "id": "9",
+        "facility": "National Energy Technology Lab",
+        "city": "Morgantown",
+        "state": " West Virginia",
+        "year": "1910",
+        "employ": "1,500",
+        "txt": "High-efficiency boilers, turbines, fuel cells and other power systems; emissions controls for coal-fired power plants; carbon capture and storage; efficiency and environmentalquality of domestic oil and natural gas exploration, production and processing; and materials for extreme environments",
+        "URL": "http://www.netl.doe.gov/",
+        "facility2": "National Energy Technology Laboratory"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -79.985953,
+          40.326042
+        ]
+      },
+      "properties": {
+        "id": "10",
+        "facility": "National Energy Technology Lab",
+        "city": "Pittsburgh",
+        "state": " Pennsylvania",
+        "year": "1910",
+        "employ": "1,500",
+        "txt": "High-efficiency boilers, turbines, fuel cells and other power systems; emissions controls for coal-fired power plants; carbon capture and storage; efficiency and environmentalquality of domestic oil and natural gas exploration, production and processing; and materials for extreme environments",
+        "URL": "http://www.netl.doe.gov/",
+        "facility2": "National Energy Technology Laboratory"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -123.1201199,
+          44.62342861
+        ]
+      },
+      "properties": {
+        "id": "11",
+        "facility": "National Energy Technology Lab",
+        "city": "Albany",
+        "state": " Oregon",
+        "year": "1910",
+        "employ": "1,500",
+        "txt": "High-efficiency boilers, turbines, fuel cells and other power systems; emissions controls for coal-fired power plants; carbon capture and storage; efficiency and environmentalquality of domestic oil and natural gas exploration, production and processing; and materials for extreme environments",
+        "URL": "http://www.netl.doe.gov/",
+        "facility2": "National Energy Technology Laboratory"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -105.151784,
+          39.744418
+        ]
+      },
+      "properties": {
+        "id": "12",
+        "facility": "National Renewable Energy Lab",
+        "city": "Golden",
+        "state": " Colorado",
+        "year": "1977",
+        "employ": "2,500",
+        "txt": "Renewable energy and energy efficiency research and development, including energy systems integration, solar, wind, renewable fuels and vehicle systems, buildings, geothermal, energy sciences, computational sciences and energy analysis",
+        "URL": "http://www.nrel.gov/",
+        "facility2": "National Renewable Energy Laboratory"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -84.3324881,
+          35.9197708
+        ]
+      },
+      "properties": {
+        "id": "13",
+        "facility": "Oak Ridge National Lab",
+        "city": "Oak Ridge",
+        "state": " Tennessee",
+        "year": "1943",
+        "employ": "4,400",
+        "txt": "Energy-related science and technology, from basic research to the development and demonstration of breakthrough materials, processes and systems",
+        "URL": "http://www.ornl.gov/",
+        "facility2": "Oak Ridge National Laboratory"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -119.2882831,
+          46.3437578
+        ]
+      },
+      "properties": {
+        "id": "14",
+        "facility": "Pacific Northwest National Lab",
+        "city": "Richland",
+        "state": " Washington",
+        "year": "1965",
+        "employ": "4,500",
+        "txt": "Chemical and molecular sciences, biological systems science; climate change science; subsurface science; chemical engineering; applied materials science and engineering; and applied nuclear science and technology",
+        "URL": "http://www.pnnl.gov/",
+        "facility2": "Pacific Northwest National Laboratory"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -74.6045356,
+          40.3493556
+        ]
+      },
+      "properties": {
+        "id": "15",
+        "facility": "Princeton Plasma Physics Lab",
+        "city": "Plainsboro",
+        "state": " New Jersey",
+        "year": "1951",
+        "employ": "450",
+        "txt": "Nuclear fusion and plasma physics",
+        "URL": "http://www.pppl.gov/",
+        "facility2": "Princeton Plasma Physics Laboratory"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -121.7211155,
+          37.6798239
+        ]
+      },
+      "properties": {
+        "id": "16",
+        "facility": "Sandia National Labs",
+        "city": "Livermore",
+        "state": " California",
+        "year": "1945",
+        "employ": "10,000",
+        "txt": "Nuclear weapons; defense; energy; materials science and homeland security; nonproliferation; supercomputing and cybersecurity; robotics; climate and infrastructure security; nuclear reactor safety; nanodevices and microsystems; geosciences; bioscience; radiation effects; and nuclear fusion.",
+        "URL": "http://www.sandia.gov/",
+        "facility2": "Sandia National Laboratories"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -106.534688,
+          35.060599
+        ]
+      },
+      "properties": {
+        "id": "17",
+        "facility": "Sandia National Labs",
+        "city": "Albuequerque",
+        "state": " New Mexico",
+        "year": "1945",
+        "employ": "10,000",
+        "txt": "Nuclear weapons; defense; energy; materials science and homeland security; nonproliferation; supercomputing and cybersecurity; robotics; climate and infrastructure security; nuclear reactor safety; nanodevices and microsystems; geosciences; bioscience; radiation effects; and nuclear fusion.",
+        "URL": "http://www.sandia.gov/",
+        "facility2": "Sandia National Laboratories"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -81.722859,
+          33.525798
+        ]
+      },
+      "properties": {
+        "id": "18",
+        "facility": "Savannah River National Lab",
+        "city": "Aiken",
+        "state": " South Carolina",
+        "year": "1951",
+        "employ": "825",
+        "txt": "Environmental remediation and risk reduction; nuclear materials processing and disposition; nuclear detection, characterization and assessments; gas processing, storage and transfer systems",
+        "URL": "http://srnl.doe.gov/",
+        "facility2": "Savannah River National Laboratory"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -122.191254,
+          37.4272378
+        ]
+      },
+      "properties": {
+        "id": "19",
+        "facility": "SLAC National Accelerator Lab",
+        "city": "Menlo Park",
+        "state": " California",
+        "year": "1962",
+        "employ": "2,000",
+        "txt": "Accelerator research; astrophysics and cosmology; biology; elementary particle physics; environmental science; materials, chemistry and energy sciences; scientific computing; and X-ray science",
+        "URL": "https://www6.slac.stanford.edu/",
+        "facility2": "SLAC National Accelerator Laboratory"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -76.518833,
+          37.136854
+        ]
+      },
+      "properties": {
+        "id": "20",
+        "facility": "Thomas Jefferson National Accelerator Facility",
+        "city": "Newport News",
+        "state": " Virginia",
+        "year": "1987",
+        "employ": "800",
+        "txt": "Experimental nuclear physics, computational and theoretical nuclear physics, accelerator science, cryogenics, superconducting radio frequency technologies, radiation detectors, medical imaging devices and free-electron lasers",
+        "URL": "https://www.jlab.org/",
+        "facility2": "Thomas Jefferson National Accelerator Facility"
+      }
+    }
+]
